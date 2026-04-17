85f36c5597edbff0756b0fd3096b1ca1d16a146b
wanghaitao8118 <wanghaitao8118@163.com>
2026-04-17 20:22:23 +0800

    remo

--- run.bat
+++ run.bat
@@ -7,6 +7,12 @@ echo.
 echo Please wait... The browser will open automatically.
 echo Do not close this black window while using the tool.
 echo.
+:: Clear proxy environment variables to ensure direct connection
+set HTTP_PROXY=
+set HTTPS_PROXY=
+set http_proxy=
+set https_proxy=
+set NO_PROXY=localhost,127.0.0.1
 
 :: Automatically open the default system browser
 start http://localhost:3000
